99208561f141aa251a2a3513ddf12decea79ffaf
Sergio Sánchez Ramírez <15837247+mofeing@users.noreply.github.com>
2020-05-07 13:06:44 +0200

    Add trace configurations

---
+++ trace/omp.strong.8threads/trace.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=trace.omp.8
+#SBATCH --workdir=.
+#SBATCH --output=job.out
+#SBATCH --error=job.err
+#SBATCH --time=00:05:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --threads-per-core=1
+
+source ${EXTRAE_HOME}/etc/extrae.sh
+export EXTRAE_CONFIG_FILE=extrae.xml
+export LD_PRELOAD=${EXTRAE_HOME}/lib/libomptrace.so
+export OMP_NUM_THREADS=8
+${HOME}/project/miniqmc/build/OMP/bin/miniqmc -g "2 2 2" -w 48
