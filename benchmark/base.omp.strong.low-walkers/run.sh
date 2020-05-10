e76a61e9af95d21de6c74a90e1b35c04a119742a
Sergio Sánchez Ramírez <15837247+mofeing@users.noreply.github.com>
2020-05-10 21:12:53 +0200

    Benchmark nested parallelism by generation of work

---
+++ benchmark/base.omp.strong.low-walkers/run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH --job-name="base.omp.strong.low-walkers"
+#SBATCH --workdir=.
+#SBATCH --output=run-%j.out
+#SBATCH --error=run-%j.err
+#SBATCH --time=01:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=48
+#SBATCH --threads-per-core=1
+
+OMP_NUM_THREADS=1 /home/pptm71/pptm71019/project/miniqmc/build/OMP/bin/miniqmc -g "2 2 2" -w 8
+mv info_2_2_2.xml time-222-1.xml
+
+OMP_NUM_THREADS=2 /home/pptm71/pptm71019/project/miniqmc/build/OMP/bin/miniqmc -g "2 2 2" -w 8
+mv info_2_2_2.xml time-222-2.xml
+
+OMP_NUM_THREADS=4 /home/pptm71/pptm71019/project/miniqmc/build/OMP/bin/miniqmc -g "2 2 2" -w 8
+mv info_2_2_2.xml time-222-4.xml
+
+OMP_NUM_THREADS=8 /home/pptm71/pptm71019/project/miniqmc/build/OMP/bin/miniqmc -g "2 2 2" -w 8
+mv info_2_2_2.xml time-222-8.xml
+
+OMP_NUM_THREADS=16 /home/pptm71/pptm71019/project/miniqmc/build/OMP/bin/miniqmc -g "2 2 2" -w 8
+mv info_2_2_2.xml time-222-16.xml
+
+OMP_NUM_THREADS=24 /home/pptm71/pptm71019/project/miniqmc/build/OMP/bin/miniqmc -g "2 2 2" -w 8
+mv info_2_2_2.xml time-222-24.xml
+
+OMP_NUM_THREADS=48 /home/pptm71/pptm71019/project/miniqmc/build/OMP/bin/miniqmc -g "2 2 2" -w 8
+mv info_2_2_2.xml time-222-48.xml
+
