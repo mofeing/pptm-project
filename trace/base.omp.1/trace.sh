415b9cf487cedf2efa86692cfe38c0e925de77ee
Sergio Sánchez Ramírez <15837247+mofeing@users.noreply.github.com>
2020-05-15 19:28:10 +0200

    Trace OpenMP parallelization

---
+++ trace/base.omp.1/trace.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=trace-base.omp.1
+#SBATCH --workdir=.
+#SBATCH --output=job.out
+#SBATCH --error=job.err
+#SBATCH --time=00:05:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --threads-per-core=1
+
+source ${EXTRAE_HOME}/etc/extrae.sh
+export EXTRAE_CONFIG_FILE=extrae.xml
+export LD_PRELOAD=${EXTRAE_HOME}/lib/libomptrace.so
+${HOME}/project/miniqmc/build/OMP/bin/miniqmc -g "2 2 2"
