3190e1ed5dc764b726525d4368f6f8a0b5244a72
Sergio Sánchez Ramírez <15837247+mofeing@users.noreply.github.com>
2020-06-12 10:07:44 +0200

    Trace ch1 with 8 and 48 threads

---
+++ trace/ch1.omp.48/trace.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=trace-ch1.omp.48
+#SBATCH --workdir=.
+#SBATCH --output=job.out
+#SBATCH --error=job.err
+#SBATCH --time=00:05:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=48
+#SBATCH --threads-per-core=1
+
+source ${EXTRAE_HOME}/etc/extrae.sh
+export EXTRAE_CONFIG_FILE=extrae.xml
+export LD_PRELOAD=${EXTRAE_HOME}/lib/libomptrace.so
+${HOME}/project/miniqmc/build/omp/bin/miniqmc -g "2 2 2"
